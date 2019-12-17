6a5029cc3c7c659687f5e42962c0917cebbb894f
Anton Osipov <anton@osipov.website>
2019-12-17 23:38:26 +0500

    First big commit

--- src/api/apiCredentials.js
+++ src/api/apiCredentials.js
@@ -1,6 +1,6 @@
 export default {
-    client_id: 3,
+    client_id: 4,
     grant_type: 'password',
-    client_secret: 'hcMlQHxPwQOixzYiySKb9TysbMCJMqt8mkrqgpaQ',
+    client_secret: 'q4GZDtfMnNsTHYodO6zsA0SV92ZZMkMr2UaEF8f4',
     scope: ''
 }
