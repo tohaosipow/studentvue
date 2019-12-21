81e1d2bc2f1998a0b494f8e26fe7a079fab0a435
Anton Osipov <anton@osipov.website>
2019-12-21 18:55:43 +0500

    Баллы командам

---
+++ src/components/Events/EventPointComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container flat color="basil">
+        <EventTeamPointSetComponent v-if="$store.state.events.currentEvent.teams_allowed"></EventTeamPointSetComponent>
+        <EventUserPointSetComponent v-else></EventUserPointSetComponent>
+    </v-container>
+</template>
+
+<script>
+    import EventUserPointSetComponent from "@/components/Events/EventUserPointSetComponent";
+    import EventTeamPointSetComponent from "@/components/Events/EventTeamPointSetComponent";
+
+    export default {
+        name: "EventPointComponent",
+        components:{
+            EventTeamPointSetComponent,
+            EventUserPointSetComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import EventItemComponent from "@/components/Events/EventItemComponent";
 import AuthComponent from "@/components/AuthComponent";
 import TeamComponent from "@/components/Events/TeamComponent";
+import EventPointComponent from "@/components/Events/EventPointComponent";
 
 
 const routes = [
@@ -10,6 +11,7 @@ const routes = [
     { path: '/events/my', component: EventsComponent, name: 'user.events', props: { my: true}},
     { path: '/events/:id', component: EventItemComponent, name: 'events.item'},
     { path: '/events/:id/teams/:team_id', component: TeamComponent, name: 'teams.item'},
+    { path: '/points', component: EventPointComponent, name: 'points'},
     { path: '/auth', component: AuthComponent },
 ]
 
