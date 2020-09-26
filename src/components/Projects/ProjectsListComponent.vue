937c3853a244274e52984dca5ded98b6d967da4a
Anton Osipov <tohaosipow@yandex.ru>
2020-09-27 00:35:49 +0500

    Redesign

--- src/components/Projects/ProjectsListComponent.vue
+++ src/components/Projects/ProjectsListComponent.vue
@@ -51,7 +51,7 @@
                         <v-list-item-content>
                             <div class="overline mb-4">{{project.type.name}} проект</div>
                             <v-list-item-title class="headline mb-1" style="white-space: normal">{{project.title}}</v-list-item-title>
-                            <v-list-item-subtitle>{{project.task}}</v-list-item-subtitle>
+                            <v-list-item-subtitle style="-webkit-line-clamp: 4">{{project.task}}</v-list-item-subtitle>
                         </v-list-item-content>
 
                         <v-list-item-avatar
