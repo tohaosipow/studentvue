4e6f676c6b4c7d55f336737518d292d77747b636
Anton Osipov <tohaosipow@yandex.ru>
2020-12-16 02:38:16 +0500

    CompleteProfile.vue

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,11 @@
                     v-model="drawer"
             >
                 <div class="text-center pa-4 text-white" style="color: white">
-                    <h4>АИС "Студент СурГУ"
+                    <h4 style="display: flex; align-items: center">
+                        <img class="mr-2" width="30px"
+                             src="https://www.apmo.ru/upload/iblock/803/803fe47506d5aa6b19079797f3f615b0.png"/>
+                        АИС "Студент СурГУ"
+
                         <v-icon @click.stop="drawer = !drawer" right
                                 style="position: absolute; right: 10px;" v-if="drawer && $vuetify.breakpoint.mdAndDown">
                             mdi-chevron-left
@@ -47,14 +51,29 @@
                         dense
                         nav
                 >
+                    <Snowf
+                            :amount="50"
+                            :size="5"
+                            :speed="1.5"
+                            :wind="0"
+                            :opacity="0.8"
+                            :swing="1"
+                            :image="null"
+                            :zIndex="-1"
+                            :resize="true"
+                            color="#fff"
+                    />
+
+
                     <v-list-item two-line v-if="$store.state.user.currentUser.id > 0">
+
 
                         <v-list-item-avatar style="overflow: visible">
 
-                        <v-badge
-                                color="purple"
-                                :content="$store.state.user.currentUser.score"
-                                offset-x="10"
+                            <v-badge
+                                    color="purple"
+                                    :content="$store.state.user.currentUser.score"
+                                    offset-x="10"
                                 offset-y="10"
                         >
                             <v-avatar size="40">
@@ -163,141 +182,6 @@
             <v-footer app>
                 <!-- -->
             </v-footer>
-            <!-- <v-navigation-drawer color="blue darken-4"
-                                  v-model="drawer"
-                                  app
-                                  dark
-             >
-
-                 <v-list-item @click="$router.push('/profile/'+$store.state.user.currentUser.id+'/edit')"  v-if="$store.state.user.currentUser.id > 0">
-                     <v-list-item-avatar>
-                         <v-img :src="$store.state.user.currentUser.avatar"></v-img>
-                     </v-list-item-avatar>
-
-                     <v-list-item-content>
-                         <v-list-item-title class="title">
-                             {{$store.state.user.currentUser.first_name}} {{$store.state.user.currentUser.last_name}}
-                         </v-list-item-title>
-                         <v-list-item-subtitle>
-                             <span v-if="$store.state.user.currentUser.role === 'visitor'">Посетитель</span>
-                             <span v-if="$store.state.user.currentUser.role === 'student'">Студент</span>
-                             <span v-if="$store.state.user.currentUser.role === 'pupil'">Учащийся</span>
-                             <span v-if="$store.state.user.currentUser.role === 'employee'">Сотрудник</span>
-                             <span v-if="$store.state.user.currentUser.role === 'company'">Партнер</span>
-                         </v-list-item-subtitle>
-                     </v-list-item-content>
-
-
-                 </v-list-item>
-
-                 <v-divider></v-divider>
-
-                 <v-list nav>
-                     <v-subheader>Мероприятия</v-subheader>
-                     <v-list-item to="/" @click="() => {}">
-                         <v-list-item-icon>
-                             <v-icon>mdi-calendar-multiple</v-icon>
-                         </v-list-item-icon>
-
-                         <v-list-item-content>
-                             <v-list-item-title>Все мероприятия</v-list-item-title>
-                         </v-list-item-content>
-
-                     </v-list-item>
-                     <v-list-item to="/events/my" v-if="$store.state.user.currentUser.id > 0">
-                         <v-list-item-icon>
-                             <v-icon>mdi-account</v-icon>
-                         </v-list-item-icon>
-
-                         <v-list-item-content>
-                             <v-list-item-title>Мои мероприятия</v-list-item-title>
-                         </v-list-item-content>
-
-                     </v-list-item>
-                     <v-subheader>Учебная часть</v-subheader>
-                     <v-list-item to="/timetable">
-                         <v-list-item-icon>
-                             <v-icon>mdi-account-network</v-icon>
-                         </v-list-item-icon>
-
-                         <v-list-item-content>
-                             <v-list-item-title>Все пары</v-list-item-title>
-                         </v-list-item-content>
-
-
-                     </v-list-item>
-                     <v-list-item to="/timetable/my" v-if="$store.state.user.currentUser.id > 0">
-                         <v-list-item-icon>
-                             <v-icon>mdi-account</v-icon>
-                         </v-list-item-icon>
-
-                         <v-list-item-content>
-                             <v-list-item-title>Мои пары</v-list-item-title>
-                         </v-list-item-content>
-
-
-                     </v-list-item>
-                     <v-subheader>Проектная деятельность</v-subheader>
-                     <v-list-item to="/projects">
-                         <v-list-item-icon>
-                             <v-icon>mdi-account-network</v-icon>
-                         </v-list-item-icon>
-
-                         <v-list-item-content>
-                             <v-list-item-title>Проекты</v-list-item-title>
-                         </v-list-item-content>
-
-
-                     </v-list-item>
-                     <template v-if="$store.state.user.currentUser.admin === 1">
-                         <v-subheader>Администратор</v-subheader>
-                         <v-list-item to="/admin/users">
-                             <v-list-item-icon>
-                                 <v-icon>mdi-account-group</v-icon>
-                             </v-list-item-icon>
-
-                             <v-list-item-content>
-                                 <v-list-item-title>Пользователи</v-list-item-title>
-                             </v-list-item-content>
-
-                         </v-list-item>
-                         <v-list-item @click="() => {}">
-                             <v-list-item-icon>
-                                 <v-icon>mdi-settings</v-icon>
-                             </v-list-item-icon>
-
-                             <v-list-item-content>
-                                 <v-list-item-title>Настройки</v-list-item-title>
-                             </v-list-item-content>
-
-                         </v-list-item>
-                     </template>
-                 </v-list>
-
-             </v-navigation-drawer> !-->
-
-            <!--<v-app-bar elevate-on-scroll app color="blue" dark
-            >
-                <v-app-bar-nav-icon @click.stop="drawer = !drawer">
-                    <v-icon v-if="drawer">
-                        mdi-close
-                    </v-icon>
-                    <v-icon v-else>
-                        mdi-menu
-                    </v-icon>
-                </v-app-bar-nav-icon>
-                <v-toolbar-title>
-                    <span class="title">{{title}}</span>
-                </v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-toolbar-items>
-                    <v-btn v-if="$store.state.user.currentUser.id > 0" @click="$store.dispatch('logout'); $router.push('/auth')" text>Выйти</v-btn>
-                    <template v-else>
-                        <v-btn to="/auth" text>Вход / Регистрация</v-btn>
-                    </template>
-                </v-toolbar-items>
-
-            </v-app-bar> !-->
 
         </v-app>
 
@@ -308,10 +192,11 @@
 
 
     import CompleteProfile from "@/components/Profile/CompleteProfile.vue";
+    import Snowf from 'vue-snowf';
 
     export default {
         name: 'app',
-        components: {CompleteProfile},
+        components: {CompleteProfile, Snowf},
         created() {
         },
         mounted() {
